f67d42f2a4891973aba53d45817725bcd31993fa
IudexSoren <aaronsibajam@gmail.com>
2022-11-18 22:12:43 -0600

    Sizes types added

--- src/common/other/types.ts
+++ src/common/other/types.ts
@@ -1,5 +1,5 @@
-export type BASIC_SIZES = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
-export type SIZES = BASIC_SIZES | 'xl' | '2xl'
+export type BASIC_SIZES = 'xs' | 'sm' | 'md' | 'lg'
+export type SIZES = BASIC_SIZES | 'xl' | '2xl' | '3xl' | '4xl'
 
 
 export interface BackgroundAttr {
