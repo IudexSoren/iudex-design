d285351ebf5d87b58415bc80d55928f52dbf94fe
IudexSoren <aaronsibajam@gmail.com>
2022-11-26 18:32:20 -0600

    NumberInput: Removed onInputChange function

--- src/common/inputs/components/NumberInput.tsx
+++ src/common/inputs/components/NumberInput.tsx
@@ -7,22 +7,9 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps>(
   max,
   min,
   onBeforeInput,
-  onChange,
   value = 0,
   ...props
 }, ref) => {
-
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-    const { value } = target;
-    const convertedValue = Number(value);
-
-    if (isNaN(convertedValue)) return;
-
-    if (!onChange) return;
-
-    onChange(event);
-  }
 
   const onBeforeInputHandler = (event: React.FormEvent<HTMLInputElement>) => {
     const allowedCharacters = ['-', '.', ','];
@@ -47,7 +34,6 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps>(
 
   return (
     <TextInput
-      onChange={onInputChange}
       onBeforeInput={onBeforeInputHandler}
       ref={ref}
       {...props}
