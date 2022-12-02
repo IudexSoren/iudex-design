78d0a42abbdbe4b1f20bd249314eceee6ac984cd
IudexSoren <aaronsibajam@gmail.com>
2022-12-02 14:55:14 -0600

    Typography levels added: span & div

--- src/common/typographies/types.ts
+++ src/common/typographies/types.ts
@@ -1,7 +1,7 @@
 import { AllHTMLAttributes } from "react"
 import { SIZES } from '@common/other/types'
 
-type TYPOGRAPHY_LEVEL = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+type TYPOGRAPHY_LEVEL = 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
 
 export interface TypographyProps extends Omit<AllHTMLAttributes<HTMLHeadingElement | HTMLParagraphElement>, 'size'> {
   level?: TYPOGRAPHY_LEVEL
