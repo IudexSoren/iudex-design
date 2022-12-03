9c516b95e066e7f9a4bf15ac8addfac7422168df
IudexSoren <aaronsibajam@gmail.com>
2022-12-03 16:22:51 -0600

    Calendar showing up for the current month

--- src/common/inputs/components/DateTimePicker.tsx
+++ src/common/inputs/components/DateTimePicker.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { DateTime, Info } from 'luxon'
-import moment from 'moment';
+import { Button } from '@common/buttons'
 import { TextInput } from './TextInput'
 import { DateTimePickerDisplay } from './datetime-picker'
 
 const WEEKS_TO_SHOW = 6;
+const DAYS_PER_WEEK = 7;
 
 export const DateTimePicker: React.FC = () => {
-
 
   const [date, setDate] = React.useState(DateTime.local());
   const [weekdaysName, setWeekdaysName] = React.useState<string[]>([]);
 
   React.useEffect(() => {
-    console.log(date);
-    setWeekdaysName(Info.weekdays('short'));
+    setWeekdaysName(Info.weekdays('narrow'));
+    renderWeeks()
   }, []);
 
   const renderWeekdaysName = React.useCallback(() => (
     weekdaysName.map((dayName, index) => (
       <th
-        className='capitalize font-medium p-1'
+        className='capitalize font-medium p-2'
         key={index}
       >
         {dayName}
@@ -28,9 +28,50 @@ export const DateTimePicker: React.FC = () => {
     ))
   ), [weekdaysName]);
 
-  // const renderWeeks = React.useCallback(() => (
+  const renderWeeks = React.useCallback(() => {
+    const monthFirstWeekday = getFirstWeekdayOfTheMonth();
 
-  // ), []);
+    const weeks = [];
+    let days = [];
+
+    let monthDay = 0;
+    for (let week = 1; week <= WEEKS_TO_SHOW; week++) {
+      for (let day = 1; day <= DAYS_PER_WEEK; day++) {
+        if ((week === 1 && day < monthFirstWeekday) || monthDay === date.endOf("month").day) {
+          days.push((
+            <td key={day}>
+
+            </td>
+          ));
+
+          continue;
+        }
+
+        monthDay++;
+        days.push((
+          <td
+            key={day}
+          >
+            <Button
+              className='btn-ghost !p-2 w-full'
+            >
+              {monthDay.toString()}
+            </Button>
+          </td>
+        ));
+      }
+      weeks.push((
+        <tr
+          key={week}
+        >
+          {days}
+        </tr>
+      ));
+      days = [];
+    }
+
+    return weeks;
+  }, [date]);
 
   const getFirstWeekdayOfTheMonth = () => {
     return date.startOf("month").weekday;
@@ -49,14 +90,18 @@ export const DateTimePicker: React.FC = () => {
         <div
           className='bg-base-200 p-3'
         >
-          <table>
+          <table
+
+          >
             <thead>
-              <tr>
+              <tr
+                className='select-none'
+              >
                 {renderWeekdaysName()}
               </tr>
             </thead>
             <tbody>
-
+              {renderWeeks()}
             </tbody>
           </table>
         </div>
